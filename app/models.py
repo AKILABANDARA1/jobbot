25bda3a2c277dd732f20fe1693a184b21d4b2845
AKILA BANDARA <akilaprabash96@gmail.com>
2025-05-06 21:27:20 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.config import Config
+
+Base = declarative_base()
+
+class UserPreference(Base):
+    __tablename__ = 'preferences'
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    keywords = Column(String)
+
+engine = create_engine(Config.DB_URL)
+SessionLocal = sessionmaker(bind=engine)
 
