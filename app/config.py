c9bbcac5bfb94c29efe96060d14c3945f03907ce
AKILA BANDARA <akilaprabash96@gmail.com>
2025-05-06 21:26:02 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    DB_URL = os.getenv("DATABASE_URL", "sqlite:///./jobs.db")
+    EMAIL_HOST = os.getenv("EMAIL_HOST")
+    EMAIL_PORT = os.getenv("EMAIL_PORT")
+    EMAIL_USER = os.getenv("EMAIL_USER")
+    EMAIL_PASS = os.getenv("EMAIL_PASS")
