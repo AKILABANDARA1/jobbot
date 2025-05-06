d06faae7bf1ee069d84e5e2becbb80bcb2dd84de
AKILA BANDARA <akilaprabash96@gmail.com>
2025-05-06 21:26:52 +0530

    Update matcher.py

--- app/matcher.py
+++ app/matcher.py
@@ -1,1 +1,7 @@
+def match_jobs(user_preferences, scraped_jobs):
+    matched = []
+    for job in scraped_jobs:
+        if job['title'].lower() in user_preferences['keywords']:
+            matched.append(job)
+    return matched
 
